8a4d05e6e8df1ba182f7a994d4a6818aa5a60a6b
André B <andre@bacasable-lab.fr>
2016-06-21 17:21:32 +0200

    wip

--- js/hearts.js
+++ js/hearts.js
@@ -4,7 +4,7 @@ $(".viewer-action").on("click", function() {
     var d = ["flowOne", "flowTwo", "flowThree", "flowFour", "flowFive", "flowSix"];
     var a = ["colOne", "colTwo", "colThree", "colFour", "colFive", "colSix"];
     var c = (Math.random() * (1.6 - 1.2) + 1.2).toFixed(1);
-    $('<div class="heart part-' + b + " " + a[Math.floor((Math.random() * a.length))] + '" style="font-size:' + Math.floor(Math.random() * (50 - 22) + 22) + 'px;"><i class="fa fa-' + item + '"></i></div>').appendTo(".hearts").css({
+    $('<div class="heart part-' + b + " " + a[Math.floor((Math.random() * a.length))] + '" style="font-size:' + Math.floor(Math.random() * (50 - 22) + 22) + 'px;"><i class="icon icon-' + item + '"></i></div>').appendTo(".hearts").css({
         animation: "" + d[Math.floor((Math.random() * d.length))] + " " + c + "s linear"
     });
     $(".part-" + b).show();
