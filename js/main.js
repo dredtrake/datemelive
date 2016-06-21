52912c2ca7f99a6f54721cb23227c1300efbb093
Kamil Molendys <kamilmolendys@gmail.com>
2016-06-21 14:38:59 +0200

    changes

---
+++ js/main.js
@@ -0,0 +1,52 @@
+function countdown(elementName, minutes, seconds, callback) {
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits(n) {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer() {
+        msLeft = endTime - (+new Date);
+        if (msLeft < 1000) {
+            callback()
+        } else {
+            time = new Date(msLeft);
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+            setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+        }
+    }
+
+    element = document.getElementById(elementName);
+    endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
+    updateTimer();
+}
+
+$(document).ready(function () {
+    countdown("countdown", 0, 3, function () {
+        $(".left.video").show();
+        $("#countdown").hide();
+        $(".connections").fadeIn();
+    } );
+
+    countdown("fake-count", 0, 7, function () {
+        $('.live li.hidden').first().fadeIn('slow').removeClass('hidden');
+    } );
+
+    countdown("fake-count", 0, 9, function () {
+        $('.live li.hidden').first().fadeIn('slow').removeClass('hidden');
+    } );
+
+    countdown("fake-count", 0, 12, function () {
+        $('.live li.hidden').first().fadeIn('slow').removeClass('hidden');
+    } );
+
+    countdown("fake-count", 0, 12, function () {
+        $('.live li.hidden').first().fadeIn('slow').removeClass('hidden');
+    } );
+
+    countdown("fake-count", 0, 30, function () {
+        $('.live li.hidden').first().fadeIn('slow').removeClass('hidden');
+    } );
+});
